3d773aeb6cf675ddbec9db9cfd423153c39f4c08
Udev2002 <udayanga.dev.lk@gmail.com>
2024-11-15 00:06:36 -0800

    chanege dashboard

--- frontend/src/components/PresenterDashboard/PresenterDashBoard.jsx
+++ frontend/src/components/PresenterDashboard/PresenterDashBoard.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Sidebar from '../SIdebar/Sidebar'
+import DashboardCards from '../DashboardCards/DashboardCards'
+import PresentationList from '../PresentationList/PresentationList'
+import Header from '../Header/Header'
 
 const PresenterDashBoard = () => {
   return (
-    <div>PresenterDashBoard</div>
+    <div  className=' flex'>
+      <Sidebar/>
+  <div>
+  <Header/>
+      <DashboardCards/>
+  </div>
+      
+
+      <div>
+      <PresentationList/>
+      </div>
+      
+    
+    </div>
   )
 }
 
