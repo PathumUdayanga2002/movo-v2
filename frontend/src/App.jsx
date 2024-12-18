7ebd98e094e51b16beb6c7a07cca67534481357a
Udev2002 <udayanga.dev.lk@gmail.com>
2024-12-18 10:18:43 -0800

    add sign in drop down

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import PresenterDashBoard from "./components/PresenterDashboard/PresenterDashBoard";
+import Home from "./components/Home/Home";
+
 
 const App = () => {
   return <div>
+    <Home/>
     <PresenterDashBoard/>
   </div>;
 };
---
+++ frontend/src/Presenter/Signin/SigninPresenter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SigninPresenter = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default SigninPresenter
