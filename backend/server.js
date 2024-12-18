626bbc407c91c79c5433e55bacf275b3adbd60dc
Udev2002 <udayanga.dev.lk@gmail.com>
2024-12-18 08:48:06 -0800

    connect database

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,23 @@ app.use(cors());
 //use  body parser
 app.use(bodyParser.json());
 
+const URL = process.env.MONGODB_URL;
+
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true, // Correct spelling
+    useUnifiedTopology: true, // Correct spelling
+  })
+  .then(() => console.log("MongoDB connected successfull..."))
+  .catch((err) => console.log(err.massage));
+
+//create connecttion
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("mongoDB connection establish successfull");
+});
+
 // app load the port
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
