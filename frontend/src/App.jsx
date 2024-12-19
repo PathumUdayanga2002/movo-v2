c3959f7f4f7bd5c2fbba689fd89d18953c3aca79
Udev2002 <udayanga.dev.lk@gmail.com>
2024-12-18 21:44:03 -0800

    add sign in admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import PresenterDashBoard from "./components/PresenterDashboard/PresenterDashBoard";
 import Home from "./components/Home/Home";
 import SigninPresenter from "./Presenter/Signin/SigninPresenter";
+import SignInAdmin from "./Admin/SignInAdmin/SignInAdmin";
 
 
 const App = () => {
   return <div>
+    <SignInAdmin/>
     <SigninPresenter/>
     <Home/>
     <PresenterDashBoard/>
