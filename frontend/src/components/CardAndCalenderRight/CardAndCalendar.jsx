5e821fc92532570ed3aacd8e8ab92ede1a626ac2
Udev2002 <udayanga.dev.lk@gmail.com>
2024-12-20 00:29:41 -0800

    add calender to dashboard

--- frontend/src/components/CardAndCalenderRight/CardAndCalendar.jsx
+++ frontend/src/components/CardAndCalenderRight/CardAndCalendar.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import Calendar from "../Calender/Calendar";
 
 const CardAndCalendar = () => {
   return (
-    <div className=" flex flex-col space-y-[100px] justify-between items-center font-poppins ml-5 ">
+    <div className=" flex flex-col gap-4 justify-between items-center font-poppins ml-5 ">
       <div className="p-4 bg-white shadow shadow-orange-600 h-[150px] rounded-xl w-[300px] ">
         <div className="flex flex-row  justify-between">
           {/* paragraph and topic */}
@@ -14,7 +13,18 @@ const CardAndCalendar = () => {
           <div className="flex text-6xl font-bold text-orange-500">04</div>
         </div>
       </div>
-      <Calendar />
+      <div className=" flex flex-col items-center ">
+        <iframe
+          src="https://calendar.google.com/calendar/embed?src=7b60592c95df5e72fce905bda24f75c6aa7a6c70c74ff558cd1b0f774f46a254%40group.calendar.google.com&ctz=Asia%2FColombo"
+          style={{ border: "0" }}
+          width="300"
+          height="300"
+          frameborder="0"
+          scrolling="no"
+          title="Google Calendar"
+          className="rounded-lg shadow-lg"
+        ></iframe>
+      </div>
     </div>
   );
 };
